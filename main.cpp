a184c7e547166270730739672e7cfe9e78778f0b
BATMINJI <minjiconan@naver.com>
2026-03-18 11:12:14 +0900

    Image 클래스 제작

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+struct FUV
+{
+	unsigned int X;
+	unsigned int Y;
+};
+
+struct FColor
+{
+	unsigned int R;
+	unsigned int G;
+	unsigned int B;
+};
+
+class Image
+{
+	FUV UV;
+	FColor Color;
+};
+
+int main()
+{
+	
+}
